d6f98e19c9cca064fdc85ce8589ca3e382441726
Lanshissh <cabelance2@gmail.com>
2025-02-17 12:23:14 +0800

    Updated project files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-form';
+  title = 'form-app';
+  
+  // Variables to store user inputs
+  userName: string = "";  
+  email: string = "";
+  age: number | null = null;
+  address: string = "";
+  submitted: boolean = false;
+
+  // Define form group and form controls
+  formdata: FormGroup = new FormGroup({
+    userName: new FormControl(""),
+    email: new FormControl(""),
+    age: new FormControl(""),
+    address: new FormControl("")
+  });
+
+  // Function to handle form submission
+  onClickSubmit(data: { userName: string; email: string; age: number; address: string }) {
+    this.userName = data.userName;
+    this.email = data.email;
+    this.age = data.age;
+    this.address = data.address;
+    this.submitted = true; // Display the output
+  }
 }
